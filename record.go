95ec32ac8cac756ac16b4fd5c13f8e1861b96b18
Chuck Preslar <chuckpreslar@gmail.com>
2013-10-18 14:51:08 -0400

    Initial insertion.

--- record.go
+++ record.go
@@ -1,7 +1,13 @@
 package librarian
 
 import (
+	"database/sql"
 	"fmt"
+)
+
+import (
+	"github.com/chuckpreslar/codex/managers"
+	"github.com/chuckpreslar/codex/nodes"
 )
 
 type Record struct {
@@ -45,6 +51,105 @@ func (r *Record) Set(name string, value interface{}) *Record {
 
 	r.modified = append(r.modified, attribute)
 	return r
+}
+
+func (r *Record) Save() error {
+	if r.pristine {
+		return r.Insert()
+	}
+
+	return r.Update()
+}
+
+func (r *Record) Insert() error {
+	accessor := r.model.table.CodexAccessor()
+	manager := managers.Insertion(accessor.Relation())
+	binding := nodes.Binding()
+
+	var values []interface{}
+
+	for i := 0; i < len(r.modified); i++ {
+		manager.Insert(binding).Into(r.modified[i].name)
+		values = append(values, r.values[r.modified[i]])
+	}
+
+	manager.Returning(r.model.table.key)
+
+	query, err := manager.SetAdapter(Librarian.adapter).ToSql()
+
+	if nil != err {
+		return err
+	}
+
+	return r.modify(query, values...)
+}
+
+func (r *Record) Update() error {
+	return nil
+}
+
+func (r *Record) modify(query string, values ...interface{}) error {
+	var (
+		transaction *sql.Tx
+		statement   *sql.Stmt
+		rows        *sql.Rows
+		columns     []string
+		err         error
+	)
+
+	if transaction, err = Librarian.handle.Begin(); nil != err {
+		return err
+	} else if statement, err = transaction.Prepare(query); nil != err {
+		return err
+	} else if rows, err = statement.Query(values...); nil != err {
+		return err
+	} else if columns, err = rows.Columns(); nil != err {
+		return err
+	}
+
+	defer func() {
+		if nil != err {
+			transaction.Rollback()
+		} else {
+			transaction.Commit()
+		}
+
+		if nil != statement {
+			statement.Close()
+		}
+
+		if nil != rows {
+			rows.Close()
+		}
+	}()
+
+	// Should be at max returning one row,
+	// returns false when constraint errors
+	// incountered.
+	rows.Next()
+
+	buffer := make([]interface{}, len(columns))
+
+	for i := 0; i < len(buffer); i++ {
+		var item interface{}
+		buffer[i] = &item
+	}
+
+	if err = rows.Scan(buffer...); nil != err {
+		return err
+	} else {
+		for i := 0; i < len(buffer); i++ {
+			r.Set(columns[i], (*buffer[i].(*interface{})))
+		}
+
+		// Record was persisted to database,
+		// set Record's pristine flag to false
+		// and clear modified Attribute array.
+		r.pristine = false
+		r.modified = make([]*Attribute, 0)
+	}
+
+	return nil
 }
 
 func (r *Record) IsPrestine() bool {
